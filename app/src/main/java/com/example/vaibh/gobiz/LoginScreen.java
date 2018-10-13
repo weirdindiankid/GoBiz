6be311a0d880e7e7fa5c4804ed7887a4667cb6be
Pranav Raikundalia <pranavraikundalia@Pranavs-MacBook-Pro.local>
2018-10-13 16:01:30 -0400

    Sign Up page complete.

--- app/src/main/java/com/example/vaibh/gobiz/LoginScreen.java
+++ app/src/main/java/com/example/vaibh/gobiz/LoginScreen.java
@@ -1,13 +1,48 @@
 package com.example.vaibh.gobiz;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class LoginScreen extends AppCompatActivity {
-
+    private Button btnSignUp;
+    private Button btnSignIn;
+    private EditText edtEmail, edtPassword;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+        btnSignUp = (Button) findViewById(R.id.btnSignup);
+        btnSignUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
+                startActivity(i);
+            }
+        });
+
+        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+
+        btnSignIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if (VerifyIDPass()){
+                    Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
+                    startActivity(i);
+                }
+                else{
+
+                }
+            }
+        });
+    }
+
+    public boolean VerifyIDPass(){
+        edtEmail = findViewById(R.id.edtEmailSignUp);
+        edtPassword = findViewById(R.id.edtPassword);
+        return true;
     }
 }
