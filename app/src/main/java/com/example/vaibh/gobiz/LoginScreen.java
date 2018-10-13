46d177cb512ac0c877e89adc7e26142b73db62c1
vaibhav19sharma <31787798+vaibhav19sharma@users.noreply.github.com>
2018-10-13 17:23:45 -0400

    User login via email/pass @ signin activity

--- app/src/main/java/com/example/vaibh/gobiz/LoginScreen.java
+++ app/src/main/java/com/example/vaibh/gobiz/LoginScreen.java
@@ -1,21 +1,39 @@
 package com.example.vaibh.gobiz;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends AppCompatActivity {
     private Button btnSignUp;
     private Button btnSignIn;
     private EditText edtEmail, edtPassword;
+    private String strEmail, strPass;
+    private FirebaseAuth mAuth;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
-        btnSignUp = (Button) findViewById(R.id.btnSignup);
+
+        btnSignUp = findViewById(R.id.btnSignup);
+
+        edtEmail = findViewById(R.id.edtEmailSignIn);
+        edtPassword = findViewById(R.id.edtPasswordSignIn);
+
+        mAuth = FirebaseAuth.getInstance();
+
         btnSignUp.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -24,25 +42,44 @@ public class LoginScreen extends AppCompatActivity {
             }
         });
 
-        btnSignIn = (Button) findViewById(R.id.btnSignIn);
+        btnSignIn = findViewById(R.id.btnSignIn);
 
         btnSignIn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                if (VerifyIDPass()){
-                    Intent i = new Intent(getApplicationContext(),SignUpActivity.class);
-                    startActivity(i);
-                }
-                else{
-
-                }
+                    VerifyIDPass();
             }
         });
     }
 
-    public boolean VerifyIDPass(){
-        edtEmail = findViewById(R.id.edtEmailSignUp);
-        edtPassword = findViewById(R.id.edtPassword);
-        return true;
+    public void VerifyIDPass(){
+        strEmail = edtEmail.getText().toString();
+        strPass = edtPassword.getText().toString();
+
+        if(strEmail.isEmpty() || strPass.isEmpty()){
+            Toast.makeText(getApplicationContext(),"EmailID or Password is empty.", Toast.LENGTH_LONG).show();
+            return;
+        }
+        mAuth.signInWithEmailAndPassword(strEmail,strPass).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    Intent i = new Intent(getApplicationContext(), WelcomeActivity.class);
+                    startActivity(i);
+                }
+                else{
+                    Toast.makeText(getApplicationContext(),"SignIn Error. "+ task.getException(),Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+        
     }
+
+//    @Override
+//    public void onStart() {
+//        super.onStart();
+//        // Check if user is signed in (non-null) and update UI accordingly.
+//        FirebaseUser currentUser = mAuth.getCurrentUser();
+//        updateUI(currentUser);
+//    }
 }
