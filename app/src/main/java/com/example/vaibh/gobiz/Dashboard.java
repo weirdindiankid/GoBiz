be2d8e29fb55a2945b775c8ce9af004c49e4c8de
Kai Munechika <kaim808@gmail.com>
2018-11-29 16:05:12 -0500

    updated dashboard and locked app screen orientation

--- app/src/main/java/com/example/vaibh/gobiz/Dashboard.java
+++ app/src/main/java/com/example/vaibh/gobiz/Dashboard.java
@@ -1,28 +1,94 @@
 package com.example.vaibh.gobiz;
 
 import android.content.Intent;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.util.Log;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.example.vaibh.gobiz.pojos.Course;
 import com.example.vaibh.gobiz.pojos.Module;
 
-import java.io.Serializable;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Iterator;
 import java.util.List;
-import java.util.Map;
 
 public class Dashboard extends AppCompatActivity {
 
-    public HashMap<Course, List<Module>> courseModuleMap ;
+    public HashMap<Course, List<Module>> courseModuleMap;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_dashboard);
 
+        setupDashboardItems();
+    }
+
+    private void setupDashboardItems() {
+        View coursesContainer = findViewById(R.id.courses);
+        View badgesContainer = findViewById(R.id.badges);
+        View networkContainer = findViewById(R.id.network);
+        View profileContainer = findViewById(R.id.profile);
+        View settingsContainer = findViewById(R.id.settings);
+        View aboutContainer = findViewById(R.id.about);
+
+        List<View> containers = Arrays.asList(coursesContainer, badgesContainer, networkContainer, profileContainer, settingsContainer, aboutContainer);
+        List<Integer> drawablesIds = Arrays.asList(R.drawable.courses, R.drawable.badges, R.drawable.network, R.drawable.profile, R.drawable.settings, R.drawable.about);
+        List<String> labels = Arrays.asList("Courses", "Badges", "Network", "Profile", "Settings", "About");
+
+        for (int i = 0; i < containers.size(); i++) {
+            View container = containers.get(i);
+            Drawable drawable = getDrawable(drawablesIds.get(i));
+            String label = labels.get(i);
+
+            ((ImageView) container.findViewById(R.id.item_image)).setImageDrawable(drawable);
+            ((TextView) container.findViewById(R.id.item_label)).setText(label);
+        }
+
+        coursesContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoCourses(view);
+            }
+        });
+
+        badgesContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoBadges(view);
+            }
+        });
+
+        networkContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoNetwork(view);
+            }
+        });
+
+        profileContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoProfile(view);
+            }
+        });
+
+        settingsContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoSettings(view);
+            }
+        });
+
+        aboutContainer.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                gotoAbout(view);
+            }
+        });
     }
 
     public void gotoCourses(View view) {
